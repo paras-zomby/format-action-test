95685bc621082d917880dd332c0e3e955f1c7576
Clang Format <clang-format@1337z.ninja>
2022-01-16 13:17:34 +0000

    apply clang-format

--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@
 int main()
 {
     std::cout << "Hello world" << std::endl;
-//    cv::Mat img = cv::imread("../1.jpg");
-//    cv::imshow("img", img);
-//    cv::waitKey(0);
+    //    cv::Mat img = cv::imread("../1.jpg");
+    //    cv::imshow("img", img);
+    //    cv::waitKey(0);
     std::cout << "progarm is exit." << std::endl;
     return 0;
 }
