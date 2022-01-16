b8e771edc27fc92154161468a34d05faa4215b3d
LHH <765143903@qq.com>
2022-01-16 21:12:22 +0800

    the first take

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+//#include "opencv2/imgcodecs.hpp"
+//#include "opencv2/highgui.hpp"
+
+int main()
+{
+    std::cout << "Hello world" << std::endl;
+//    cv::Mat img = cv::imread("../1.jpg");
+//    cv::imshow("img", img);
+//    cv::waitKey(0);
+    std::cout << "progarm is exit." << std::endl;
+    return 0;
+}
